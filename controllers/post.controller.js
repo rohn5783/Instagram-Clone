e81e53194504f6eda198e880bd6faaf8c1892bdc
Rohit Pandey <rp5120523@gmail.com>
2026-02-14 20:40:00 +0530

    multer installed

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,7 +1,23 @@
 import post from "../model/post.model.js";
+import ImageKit from '@imagekit/nodejs';
+import {toFile} from  "@imagekit/nodejs"
+
+
+
+const imagekit = new ImageKit({
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY // This is the default and can be omitted
+});
+
+
 
 async function createPostController(req,res){
-console.log(req.body)
+console.log(req.body,req.file)
+
+const file = await imagekit.files.upload({
+    file: await toFile(Buffer.from(req.file.buffer), "file"),
+    fileName: "Test",
+})
+res.send(file)
 }
 
 
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import postController from   "../controllers/post.controller.js";
 import multer from "multer";
+const upload = multer({storage: multer.memoryStorage()});
 const postRouter = express.Router();
 
 //  post api
 
-postRouter.post("/", postController.createPostController);
+postRouter.post("/",upload.single("profile"), postController.createPostController);
 
 
 
